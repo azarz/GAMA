12c1d4a562d572c0d80848be0eae37e43ea9ce51
azarz <amaury.zarzelli@ensg.eu>
2017-12-09 16:06:07 +0100

    initial commit

---
+++ src/main/java/eu/ensg/tsi/azarzelli/gama/App.java
@@ -0,0 +1,13 @@
+package eu.ensg.tsi.azarzelli.gama;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
